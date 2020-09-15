82d9f246775cbfc76ad18a4bb1f4b14b2640eaa6
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-09-15 10:11:42 -0500

    agregamos una variable edad, para pedir, almacenar y mostrar la edad ingreasa

--- main.cpp
+++ main.cpp
@@ -3,12 +3,17 @@
 
 using namespace std;
 int main(){
-    char nombre[30];
+    char nombre[20];
+    int edad;
 
     cout<<"Hola mundo"<<endl;
     cout<<"Nombre: ";
     cin.getline(nombre,30,'\n');
     cout<<"\nNombre: "<<nombre<<endl;
+
+    cout<<"\nDigite tu edad: ";
+    cin>>edad;
+    cout<<"Edad: "<<edad<<endl;
 
     system("pause");
     return 0;
