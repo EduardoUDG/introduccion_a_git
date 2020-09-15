ed2d6dfd49cb602a0b499e2b4e7feeab888bdb7a
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-09-15 10:04:08 -0500

    Cree una variable char para pedir el nombre, guardarlo y luego mostrarlo

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@
 
 using namespace std;
 int main(){
+    char nombre[30];
 
     cout<<"Hola mundo"<<endl;
+    cout<<"Nombre: ";
+    cin.getline(nombre,30,'\n');
+    cout<<"\nNombre: "<<nombre<<endl;
 
     system("pause");
     return 0;
